66c3dab7d92265c20864b0bf88772710cd19c436
Aayush Kapoor <xeoneux@icloud.com>
2019-03-25 13:33:13 +0530

    Add sdl2 lib via conan

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
+#include <SDL.h>
 #include <iostream>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  SDL_version version;
+  SDL_GetVersion(&version);
+  std::cout << "Running SDL version " << int(version.major) << "."
+            << int(version.minor) << "." << int(version.patch) << std::endl;
+
   return 0;
 }
