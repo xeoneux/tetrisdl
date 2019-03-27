e91595979794dcbc877344bd64ca5a725a356979
Aayush Kapoor <xeoneux@icloud.com>
2019-03-28 01:28:08 +0530

    Use renderer to create a strobe effect

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <SDL.h>
 #include <iostream>
 
+auto randomColor() -> Uint8 { return random() % 255; };
+
 auto main() -> int {
 
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
@@ -15,13 +17,26 @@ auto main() -> int {
   if (window == nullptr)
     return EXIT_FAILURE;
 
+  auto renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  if (renderer == nullptr) {
+    return EXIT_FAILURE;
+  }
+
   SDL_Event event;
   auto running = true;
 
-  while (running)
+  while (running) {
+    SDL_SetRenderDrawColor(renderer, randomColor(), randomColor(),
+                           randomColor(), randomColor());
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
     while (SDL_PollEvent(&event))
       if (event.type == SDL_QUIT)
         running = false;
+  }
 
   SDL_DestroyWindow(window);
 
